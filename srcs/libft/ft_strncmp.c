a2b986996463b811e5a9f6e501b864c2f3c5d530
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-03-19 19:37:03 +0100

    Fix strncmp
    Le he metido unos buenos ciclos al strncmp para que no haga segfault. Ahora esta durisimo

--- srcs/libft/ft_strncmp.c
+++ srcs/libft/ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alberrod <alberrod@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: alberrod <alberrod@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 21:36:09 by alberrod          #+#    #+#             */
-/*   Updated: 2023/12/11 21:39:00 by alberrod         ###   ########.fr       */
+/*   Updated: 2024/03/19 19:36:03 by alberrod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	idx;
 	int		lexicographic_return;
 
+	if (!s1 && s2)
+		return (*s2);
+	if (!s2 && s1)
+		return (*s1);
 	idx = -1;
 	if (!n)
 		return (0);
