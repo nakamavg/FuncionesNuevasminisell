dd64fd38cf871929f81e398af157fb7c7c36fda2
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-04-20 12:53:54 +0200

    Build: Remove the quotes in the parser

--- srcs/parse_input/build_pipe.c
+++ srcs/parse_input/build_pipe.c
@@ -3,6 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   build_pipe.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+/*   By: alberrod <alberrod@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 12:32:14 by alberrod          #+#    #+#             */
+/*   Updated: 2024/04/20 12:48:12 by alberrod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   build_pipe.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: alberrod <alberrod@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 17:11:38 by alberrod          #+#    #+#             */
@@ -11,6 +23,27 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char **trim_quotes(char **cmd_list)
+{
+	int idx;
+	char **tmp_list;
+
+	tmp_list = ft_calloc(ft_strlen_pp(cmd_list) + 1, sizeof(char *));
+	idx = -1;
+	while (cmd_list[++idx])
+	{
+		if (cmd_list[idx][0] == '"')
+			tmp_list[idx] = ft_strtrim(cmd_list[idx], "\"");
+		else if (cmd_list[idx][0] == '\'')
+			tmp_list[idx] = ft_strtrim(cmd_list[idx], "'");
+		else
+			tmp_list[idx] = ft_strdup(cmd_list[idx]);
+		free(cmd_list[idx]);
+	}
+	free(cmd_list);
+	return (tmp_list);
+}
 
 t_cmd	*init_pipe(const char *text, size_t text_length, int initial_idx,
 	t_shell *shell)
@@ -31,6 +64,7 @@ t_cmd	*init_pipe(const char *text, size_t text_length, int initial_idx,
 		token->write_mode = ft_outfile_mode(token->text);
 	token->cmd_list = cmd_split(token->text, token->infile, token->outfile);
 	token->cmd_list = expand_variable(token->cmd_list, shell, 0, 0);
+	token->cmd_list = trim_quotes(token->cmd_list);
 	token->next_cmd = NULL;
 	token->prev_token = NULL;
 	return (token);
